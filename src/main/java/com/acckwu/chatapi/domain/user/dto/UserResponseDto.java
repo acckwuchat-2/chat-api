0ad00e8e7b703bef2a76dc24e318056ef246b559
HAN JONGSEO <pantene74@kw.ac.kr>
2025-11-13 01:52:33 +0900

    Feat: /me API 응답 포맷 수정 (email 제거 및 id 필드명 변경)

--- src/main/java/com/acckwu/chatapi/domain/user/dto/UserResponseDto.java
+++ src/main/java/com/acckwu/chatapi/domain/user/dto/UserResponseDto.java
@@ -1,11 +1,12 @@
 package com.acckwu.chatapi.domain.user.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
 
 @Data
 @AllArgsConstructor
 public class UserResponseDto {
+    @JsonProperty("id")
     private String userId;
     private String username;
-    private String email;
 }
