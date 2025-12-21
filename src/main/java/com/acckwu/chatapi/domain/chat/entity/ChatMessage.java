1e346b39a89208e093fbc0cd9f7d37a1fea907d3
woongjee <jeewoong7753@naver.com>
2025-12-22 00:16:02 +0900

    fix: 채팅방 입장 시 메시지 불러오기 오류 수정

--- src/main/java/com/acckwu/chatapi/domain/chat/entity/ChatMessage.java
+++ src/main/java/com/acckwu/chatapi/domain/chat/entity/ChatMessage.java
@@ -9,6 +9,7 @@ import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.*;
 @AllArgsConstructor
 @Builder
 public class ChatMessage {
+
     private String chatRoomId;
     private String senderId;
     private String messageId;
@@ -17,12 +18,25 @@ public class ChatMessage {
     private Long seq;
 
     @DynamoDbPartitionKey
+    @DynamoDbAttribute("chat_room_id")
     public String getChatRoomId() {
         return chatRoomId;
     }
 
     @DynamoDbSortKey
+    @DynamoDbAttribute("created_at")
     public String getCreatedAt() {
         return createdAt;
     }
+
+    @DynamoDbAttribute("sender_id")
+    public String getSenderId() {
+        return senderId;
+    }
+
+    @DynamoDbAttribute("message_id")
+    public String getMessageId() {
+        return messageId;
+    }
 }
+
